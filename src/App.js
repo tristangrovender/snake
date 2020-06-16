95330686f584b6634718a4eab805776fb694ee1e
Daniel Rasmuson <dan123911@gmail.com>
2020-06-16 12:41:06 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -4,96 +4,93 @@ import Grid from "./Grid";
 import "./App.css";
 
 function moveSnake({ direction, body }) {
-    // In functional programming we NEVER mutate - only copy and create. Commented out solution below mutates.
-    const currentHead = body[body.length - 1];
-    let newHead;
-    if (direction === "down") {
-        newHead = { ...currentHead, y: currentHead.y + 1 };
-    } else if (direction === "up") {
-        newHead = { ...currentHead, y: currentHead.y - 1 };
-    } else if (direction === "right") {
-        newHead = { ...currentHead, x: currentHead.x + 1 };
-    } else if (direction === "left") {
-        newHead = { ...currentHead, x: currentHead.x - 1 };
-    }
-    return [...body.slice(1), newHead];
+  // In functional programming we NEVER mutate - only copy and create. Commented out solution below mutates.
+  const currentHead = body[body.length - 1];
+  let newHead;
+  if (direction === "down") {
+    newHead = { ...currentHead, y: currentHead.y + 1 };
+  } else if (direction === "up") {
+    newHead = { ...currentHead, y: currentHead.y - 1 };
+  } else if (direction === "right") {
+    newHead = { ...currentHead, x: currentHead.x + 1 };
+  } else if (direction === "left") {
+    newHead = { ...currentHead, x: currentHead.x - 1 };
+  }
+  return [...body.slice(1), newHead];
 }
 
 function App() {
-    const [gameState, setGameState] = useState({
-        rowCount: 17,
-        columnCount: 17,
-        targetCircle: { x: 3, y: 15 },
-        snake: {
-            direction: "right",
-            body: [{ x: 10, y: 10 }, { x: 10, y: 11 }, { x: 10, y: 12 }]
-        },
-        frameRateInMs: 1000,
-        refreshScreen: true
+  const [gameState, setGameState] = useState({
+    rowCount: 17,
+    columnCount: 17,
+    targetCircle: { x: 3, y: 15 },
+    snake: {
+      direction: "right",
+      body: [
+        { x: 10, y: 10 },
+        { x: 10, y: 11 },
+        { x: 10, y: 12 },
+      ],
+    },
+    frameRateInMs: 1000,
+  });
+
+  setTimeout(function () {
+    setGameState({
+      ...gameState,
+      snake: {
+        ...gameState.snake,
+        body: moveSnake(gameState.snake),
+      },
+      refreshScreen: true,
     });
+  }, 1000);
 
-    if (gameState.refreshScreen) {
-        setTimeout(function() {
-            setGameState({
-                ...gameState,
-                snake: {
-                    ...gameState.snake,
-                    body: moveSnake(gameState.snake)
-                },
-                refreshScreen: true
-            });
-        }, 1000);
-        setGameState({
-            ...gameState,
-            refreshScreen: false
-        });
+  const setDirection = (direction) => {
+    setGameState({
+      ...gameState,
+      snake: {
+        ...gameState.snake,
+        direction,
+      },
+    });
+  };
+
+  // TODO is update gameState.snake.direction with the correct direction in the below handler
+  const directionHandler = ({ key }) => {
+    if (key === "ArrowUp") {
+      if (gameState.snake.direction !== "down") {
+        setDirection("up");
+      }
+    } else if (key === "ArrowDown") {
+      if (gameState.snake.direction !== "up") {
+        setDirection("down");
+      }
+    } else if (key === "ArrowRight") {
+      if (gameState.snake.direction !== "left") {
+        setDirection("right");
+      }
+    } else if (key === "ArrowLeft") {
+      if (gameState.snake.direction !== "right") {
+        setDirection("left");
+      }
     }
+  };
 
-    const setDirection = direction => {
-        setGameState({
-            ...gameState,
-            snake: {
-                ...gameState.snake,
-                direction
-            }
-        });
-    };
+  // Solution
+  window.removeEventListener("keyup", directionHandler);
+  window.addEventListener("keyup", directionHandler);
 
-    // TODO is update gameState.snake.direction with the correct direction in the below handler
-    const directionHandler = ({ key }) => {
-        if (key === "ArrowUp") {
-            if (gameState.snake.direction !== "down") {
-                setDirection("up");
-            }
-        } else if (key === "ArrowDown") {
-            if (gameState.snake.direction !== "up") {
-                setDirection("down");
-            }
-        } else if (key === "ArrowRight") {
-            if (gameState.snake.direction !== "left") {
-                setDirection("right");
-            }
-        } else if (key === "ArrowLeft") {
-            if (gameState.snake.direction !== "right") {
-                setDirection("left");
-            }
-        }
-    };
-
-    // Solution
-    window.removeEventListener("keyup", directionHandler);
-    window.addEventListener("keyup", directionHandler);
-
-    return (
-        <div className="App">
-            <Grid
-                rowCount={gameState.rowCount}
-                columnCount={gameState.columnCount}
-                snake={gameState.snake}
-                targetCircle={gameState.targetCircle}
-            />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Grid
+        rowCount={gameState.rowCount}
+        columnCount={gameState.columnCount}
+        snake={gameState.snake}
+        targetCircle={gameState.targetCircle}
+      />
+    </div>
+  );
 }
 
 export default App;
