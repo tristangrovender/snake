b6dfc49e20cb41979471b85082842acd93c0cd9a
Tristan Grovender <therealtristangrovender@gmail.com>
2020-06-19 12:01:57 -0500

    fixed target populating on snake bug

--- src/game-reducer.js
+++ src/game-reducer.js
@@ -25,7 +25,6 @@ export function setSnakeDirection(direction) {
 
 function getGridCells({ rowCount, columnCount }) {
     let gridCells = [];
-
     for (let y = 0; y < rowCount; y++) {
         for (let x = 0; x < columnCount; x++) {
             gridCells.push({ x: x, y: y });
@@ -36,37 +35,26 @@ function getGridCells({ rowCount, columnCount }) {
 }
 
 function getRandomTargetCirclePosition(gameState) {
-    // TODO: return a new random (and valid) target circle position
-    // What is a valid position?
-    // - the position is within the grid's dimensions
-    // // - has an x,y value
-    // // - the coordinate is not inside the snake body
-
-    // [x] 1. Write a function that returns an array with all the possible coordinates in the grid
-
     const grid = getGridCells(gameState);
-
-    // [ ] 2. Remove coordinates in that array that include coordinates in the snake body
-
-    // TODO
-    // grid is 289 length
-    // the snake length 3
-    // the new grid size should be 286
-    const isCellNotInSnakeBody = coordinate => {
+    const isCellNotInSnakeBody = cellInGrid => {
         // returns true or false (hint: use .some)
-        return true;
+        const cellIsInSnakeBody = gameState.snake.body.some(cellInSnakeBody => {
+            return (
+                cellInSnakeBody.x === cellInGrid.x &&
+                cellInSnakeBody.y === cellInGrid.y
+            );
+        });
+        return !cellIsInSnakeBody;
     };
     const gridWithoutSnakeBody = grid.filter(isCellNotInSnakeBody);
-    console.log("is this 286?: ", gridWithoutSnakeBody.length);
 
     // [ ] 3. Pick a random item in the array as the coordinate for the target circle
 
-    return {
-        x: Math.floor(Math.random() * 17),
-        y: Math.floor(Math.random() * 17)
-    };
+    console.log("newGrid", gridWithoutSnakeBody);
 
-    // return { x: 5, y: 5 };
+    // TODO pick a random number in the range shown below
+    const randomIndex = Math.floor(Math.random() * gridWithoutSnakeBody.length);
+    return gridWithoutSnakeBody[randomIndex];
 }
 
 function updateGameFrame(gameState) {
